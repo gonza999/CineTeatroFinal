17261591e9d268f720a866e2f97472216e9ab691
Gonzalo Misciagna <gonza999capo@gmail.com>
2021-12-24 00:16:41 -0300

    MVC-Venta Completada, Lucir detalles y agregar vista de venta realizada

--- CineTeatroItalianoLobos.Web/Controllers/VentaController.cs
+++ CineTeatroItalianoLobos.Web/Controllers/VentaController.cs
@@ -2,6 +2,7 @@
 using CineTeatroItalianoLobos.Services.Facades;
 using CineTeatroItalianoLobos.Web.Clases;
 using CineTeatroItalianoLobos.Web.Models.Carrito;
+using CineTeatroItalianoLobos.Web.Models.Empleado;
 using CineTeatroItalianoLobos.Web.Models.Evento;
 using CineTeatroItalianoLobos.Web.Models.Horario;
 using CineTeatroItalianoLobos.Web.Models.Localidad;
@@ -23,11 +24,15 @@ namespace CineTeatroItalianoLobos.Web.Controllers
         private readonly ITicketsServicio _servicioTickets;
         private readonly IHorariosServicio _servicioHorarios;
         private readonly ILocalidadesServicio _servicioLocalidades;
+        private readonly IFormasPagosServicio _servicioFormasPagos;
+        private readonly IFormasVentasServicio _servicioFormasVentas;
+
         private readonly int cantidadPorPaginas = 12;
         public VentaController(IVentasServicio servicio,
             IEmpleadosServicio servicioEmpleados,
             ITicketsServicio servicioTickets, IEventosServicios servicioEventos,
-            IHorariosServicio servicioHorarios, ILocalidadesServicio servicioLocalidades
+            IHorariosServicio servicioHorarios, ILocalidadesServicio servicioLocalidades,
+            IFormasPagosServicio servicioFormasPagos, IFormasVentasServicio servicioFormasVentas
             )
         {
             _servicio = servicio;
@@ -36,6 +41,8 @@ namespace CineTeatroItalianoLobos.Web.Controllers
             _servicioEventos = servicioEventos;
             _servicioHorarios = servicioHorarios;
             _servicioLocalidades = servicioLocalidades;
+            _servicioFormasPagos = servicioFormasPagos;
+            _servicioFormasVentas = servicioFormasVentas;
         }
         public ActionResult Index(int? empleadoSeleccionadoId = null, int? page = null)
         {
@@ -82,11 +89,15 @@ namespace CineTeatroItalianoLobos.Web.Controllers
         public ActionResult ChoiseEvent()
         {
             var carrito = new Carrito();
+            carrito.FormasPagos = Mapeador.ConstruirListaFormaPagoVm(_servicioFormasPagos.GetLista());
+            carrito.FormasVentas = Mapeador.ConstruirListaFormaVentaVm(_servicioFormasVentas.GetLista());
             carrito.Eventos = ConstruirListaEventosSinSuspender();
             carrito.Horarios = new List<HorarioListVm>();
             carrito.Localidades = new List<LocalidadListVm>();
+            carrito.Empleados = Mapeador.ConstruirListaEmpleadosVm(_servicioEmpleados.GetLista());
             carrito.EventoId = -1;
             carrito.HorarioId = -1;
+            carrito.Importe = 0;
             return View(carrito);
         }
 
@@ -115,9 +126,16 @@ namespace CineTeatroItalianoLobos.Web.Controllers
 
         public ActionResult ChoiseEvent(Carrito carrito)
         {
+            if (carrito.Importe<=0)
+            {
+                carrito.Importe = 0;
+            }
+            carrito.Empleados = Mapeador.ConstruirListaEmpleadosVm(_servicioEmpleados.GetLista());
+            carrito.FormasPagos = Mapeador.ConstruirListaFormaPagoVm(_servicioFormasPagos.GetLista());
+            carrito.FormasVentas = Mapeador.ConstruirListaFormaVentaVm(_servicioFormasVentas.GetLista());
+            carrito.Eventos = ConstruirListaEventosSinSuspender();
             if (!ModelState.IsValid)
             {
-                carrito.Eventos = ConstruirListaEventosSinSuspender();
                 carrito.Horarios = new List<HorarioListVm>();
                 carrito.Localidades = new List<LocalidadListVm>();
                 carrito.EventoId = -1;
@@ -127,7 +145,6 @@ namespace CineTeatroItalianoLobos.Web.Controllers
             var evento = _servicioEventos.GetTEntityPorId(carrito.EventoId);
             if (evento==null)
             {
-                carrito.Eventos = ConstruirListaEventosSinSuspender();
                 carrito.EventoId = -1;
                 carrito.Horarios = new List<HorarioListVm>();
                 carrito.Localidades = new List<LocalidadListVm>();
@@ -142,7 +159,6 @@ namespace CineTeatroItalianoLobos.Web.Controllers
             carrito.Horarios = listaHorariosVm;
             if (carrito.HorarioId<=0 && carrito.EventoId>0)
             {
-                carrito.Eventos = ConstruirListaEventosSinSuspender();
                 carrito.HorarioId = -1;
                 carrito.Localidades = new List<LocalidadListVm>();
                 return View(carrito);
@@ -172,8 +188,38 @@ namespace CineTeatroItalianoLobos.Web.Controllers
             {
                 l.SetearDetalles();
             }
-            carrito.Eventos = ConstruirListaEventosSinSuspender();
-            return View(carrito);
+            if (carrito.FormaPagoId<=0 && carrito.FormaVentaId<=0)
+            {
+                 return View(carrito);
+            }
+            Ticket ticket = new Ticket() 
+            { 
+                Anulada=false,
+                FechaVenta=DateTime.Now,
+                FormaPagoId=carrito.FormaPagoId,
+                FormaVentaId=carrito.FormaVentaId,
+                HorarioId=carrito.HorarioId,
+                Importe=carrito.Importe,
+                LocalidadId=carrito.LocalidadId
+            };
+            VentaTicket ventaTicket = new VentaTicket()
+            {
+                Ticket = ticket,
+            };
+            List<VentaTicket> listaVentaTickets = new List<VentaTicket>();
+            listaVentaTickets.Add(ventaTicket);
+            Venta venta = new Venta()
+            {
+                Estado = false,
+                Fecha = DateTime.Now,
+                Total = ticket.Importe,
+                VentasTickets = listaVentaTickets,
+                EmpleadoId = carrito.EmpleadoId
+            };
+            ticket.VentasTickets = listaVentaTickets;
+            ventaTicket.Venta = venta;
+            _servicio.Guardar(venta);
+            return RedirectToAction("FinVenta");
         }
 
     }
--- CineTeatroItalianoLobos.Web/Models/Carrito/Carrito.cs
+++ CineTeatroItalianoLobos.Web/Models/Carrito/Carrito.cs
@@ -1,4 +1,5 @@
-﻿using CineTeatroItalianoLobos.Web.Models.Evento;
+﻿using CineTeatroItalianoLobos.Web.Models.Empleado;
+using CineTeatroItalianoLobos.Web.Models.Evento;
 using CineTeatroItalianoLobos.Web.Models.FormaPago;
 using CineTeatroItalianoLobos.Web.Models.FormaVenta;
 using CineTeatroItalianoLobos.Web.Models.Horario;
@@ -14,28 +15,44 @@ namespace CineTeatroItalianoLobos.Web.Models.Carrito
     public class Carrito
     {
         [Display(Name ="Evento")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
         public int EventoId { get; set; }
         public EventoListVm Evento { get; set; }
         public List<EventoListVm> Eventos { get; set; }
         [Display(Name = "Horario")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
 
         public int HorarioId { get; set; }
         public HorarioListVm Horario { get; set; }
         public List<HorarioListVm> Horarios { get; set; }
         [Display(Name = "Forma de Venta")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
 
         public int FormaVentaId { get; set; }
         public FormaVentaListVm FormaVenta { get; set; }
         public List<FormaVentaListVm> FormasVentas { get; set; }
         [Display(Name = "Forma de Pago")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
 
         public int FormaPagoId { get; set; }
         public FormaPagoListVm FormaPago { get; set; }
         public List<FormaPagoListVm> FormasPagos { get; set; }
         [Display(Name = "Localidad")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
 
         public int LocalidadId { get; set; }
         public LocalidadListVm Localidad { get; set; }
         public List<LocalidadListVm> Localidades { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Range(0, int.MaxValue)]
+        public decimal Importe { get; set; }
+
+        [Display(Name = "Empleado")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        public int EmpleadoId { get; set; }
+        public EmpleadoListVm Empleado { get; set; }
+        public List<EmpleadoListVm> Empleados { get; set; }
     }
 }
